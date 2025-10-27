4824ce58136eba9f1e23cd3478af4af52503f726
KKT <kimgt2828@gmail.com>
2025-10-27 20:30:01 +0900

    First commit

---
+++ src/main/java/com/endlesspassion/sigai/SigAiApplication.java
@@ -0,0 +1,13 @@
+package com.endlesspassion.sigai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SigAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/endlesspassion/sigai/SigAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endlesspassion.sigai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
