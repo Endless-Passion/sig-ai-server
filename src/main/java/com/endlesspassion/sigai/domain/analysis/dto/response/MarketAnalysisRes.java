81d1a4d36049c74e88fe44342fc957fb90cbf3ca
KKT <kimgt2828@gmail.com>
2025-10-30 20:47:47 +0900

    Feat(#1-analysis): 상권 분석 Dto 구성 - MarketAnalysisRes: 응답 Dto - MerkeyAnalysisReq: 요청 Dto

--- src/main/java/com/endlesspassion/sigai/domain/analysis/dto/response/MarketAnalysisRes.java
+++ src/main/java/com/endlesspassion/sigai/domain/analysis/dto/response/MarketAnalysisRes.java
@@ -1,4 +1,40 @@
 package com.endlesspassion.sigai.domain.analysis.dto.response;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class MarketAnalysisRes {
+
+    private Long storeId;
+    private String storeName;
+
+    private RevenueComparisonRes revenueComparison; // 매출 비교
+    private RankingTrendRes rankingTrend; // 순위 변화 추이
+    private CompetitionIndexRes competitionIndex; // 경쟁 강도
+
+    private LocalDate analyzedAt;
+
+    public static MarketAnalysisRes of(
+            Long storeId,
+            String storeName,
+            RevenueComparisonRes revenueComparison,
+            RankingTrendRes rankingTrend,
+            CompetitionIndexRes competitionIndex
+    ) {
+        return MarketAnalysisRes.builder()
+                .storeId(storeId)
+                .storeName(storeName)
+                .revenueComparison(revenueComparison)
+                .rankingTrend(rankingTrend)
+                .competitionIndex(competitionIndex)
+                .analyzedAt(LocalDate.now())
+                .build();
+    }
 }
